0bb287dd2a28cb060d1d3155c0f16305704889f9
Krishna <kpratapsingh026@gmail.com>
2022-04-05 10:14:00 +0530

    First Commit in main branch

---
+++ Codechef April Cookoff/Chef_Goes_Shopping.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long>
+#define vvl vector<vl>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vb vector<bool>
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define mp make_pair
+#define rep(a, b, c) for (long long a = b; a <= c; ++a)
+#define repr(a, b, c) for (long long a = b; a >= c; --a)
+#define input(a, b, n)                 \
+    for (long long i = b; i <= n; ++i) \
+        cin >> a[i];
+#define output(a, b, n)                \
+    for (long long i = b; i <= n; ++i) \
+        cout << a[i] << " ";
+#define Mod 1000000007
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        vll l(n), r(n);
+        input(l, 0, n - 1);
+        input(r, 0, n - 1);
+        ll ans = 0;
+        rep(i, 0, n - 2)
+        {
+            ans += min(l[i + 1], r[i]); // Leftmost element can remove only [i+1] and Rightmost element can remove only [i] if( A(i) A(i+1) two pairs exist)
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ Codechef April Cookoff/Sticks_and_Rectangles.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vl vector<long>
+#define vvl vector<vl>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vb vector<bool>
+#define pb push_back
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define mp make_pair
+#define rep(a, b, c) for (long long a = b; a <= c; ++a)
+#define repr(a, b, c) for (long long a = b; a >= c; --a)
+#define input(a, b, n)                 \
+    for (long long i = b; i <= n; ++i) \
+        cin >> a[i];
+#define output(a, b, n)                \
+    for (long long i = b; i <= n; ++i) \
+        cout << a[i] << " ";
+#define Mod 1000000007
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int l[101];
+        memset(l, 0, sizeof(l));
+        int cnt = 0;
+        rep(i, 0, n - 1)
+        {
+            int num;
+            cin >> num;
+            l[num] += 1;
+            if (l[num] & 1)
+            {
+                ++cnt;
+            }
+            else
+                --cnt;
+        }
+        cnt += n;
+        if ((cnt / 2) & 1)
+            cnt += 2;
+        cout << cnt - n << endl;
+    }
+    return 0;
+}
